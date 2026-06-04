e8e90dae96e94c7bdf8fdd1b58eee06867f76f39
Fwaz184 <abusalmman2@gmail.com>
2026-06-04 15:14:06 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -291,7 +291,7 @@ async function startFlow(chatId, userName = "") {
 }
 
 // ─── الـ Handler الرئيسي ───
-module.exports = async function handler(req, res) { {
+module.export= default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(200).json({ ok: true, message: "Trading Bot is running ✅" });
   }
@@ -361,4 +361,3 @@ module.exports = async function handler(req, res) { {
     return res.status(200).json({ ok: true });
   }
 }
-
